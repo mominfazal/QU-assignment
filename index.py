3fd9e0cbedd0736434c4eb46b8262f5a32970579
Fazal Momin <fazal.momind25@gmail.com>
2018-01-01 08:48:56 +0530

    Bug fixes|Fazal

--- index.py
+++ index.py
@@ -39,8 +39,8 @@ def get_data_from_twitter(query,tweetID):
 	access_token_key = '847013729187352576-X0FIKTAd6VzJwCOnxxfKwgtTiKjZd2X'
 	access_token_secret = 'UKIcmA6MRuiUAL6QBrq3UvDK4hSMuAgbYPldYQdoR5ViT'
 
-	#Getting a connection with twitter using above credentials
 	try:
+		#Getting a connection with twitter using above credentials
 		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 		auth.set_access_token(access_token_key, access_token_secret)
 		api = tweepy.API(auth)
@@ -48,47 +48,35 @@ def get_data_from_twitter(query,tweetID):
 		# fetching complete timeline of the user
 		# api.user_timeline('TwitterHandle Here')
 		if(query is None):
-			query = 'realDonaldTrump'
+			query = 'realDonaldTrump' # Incase text box is blank
 
-		if(tweetID is None): # This will always be None
-			userData = api.user_timeline(query,count=25,tweet_mode="extended")    # To make this task generic we just need to make 'TwitterHandle' field dynamic
-			# dataToSend is the variable which will be returned to the User Interface
-			dataToSend['total_tweets'] = len(userData)
-			dataToSend['results'] = []
+		userData = api.user_timeline(query,count=25,tweet_mode="extended")    # To make this task generic we just need to make 'TwitterHandle' field dynamic
+		# dataToSend is the variable which will be returned to the User Interface
+		dataToSend['total_tweets'] = len(userData)
+		dataToSend['results'] = []
 
-		else: # We are not including this part due to time crunch
-			tweetData = api.get_status(tweetID)
-			dataToSend['total_tweets'] = len(tweetData)
-			dataToSend['results'] = []
-
-		#At this moment userData is actually donaldTrumpData
-
+	# else: # We are not including this part due to time crunch
+	# 	tweetData = api.get_status(tweetID)
+	# 	dataToSend['total_tweets'] = len(tweetData)
+	# 	dataToSend['results'] = []
 	except:
 		dataToSend['error_message'] = 'Coudnt connect to twitter'
 
-	if(tweetID is None): 
-		for eachData in userData:
-			try:
-				singleData = {}
-				singleData['twitter_text'] = re.sub(r'[^\x00-\x7f]',r'', eachData.full_text.encode("utf-8")) # remove hexadecimal characters
-				singleData['url'] = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', singleData['twitter_text']) #find all URLS - iframe URLS in this case
-				singleData['created_at'] = str(eachData.created_at.date())
-				singleData['id'] = str(eachData.id_str.encode("utf-8"))
-				for url in singleData['url']:
-					singleData['twitter_text'] = singleData['twitter_text'].replace(url,'').strip()  # Remove all URLS from text as we wont display it.
-				dataToSend['results'].append(singleData)
-
-			except:
-				print(sys.exc_info()[0])
-
-		return dataToSend
-	else: # Unused part
+	for eachData in userData:
 		try:
-			for eachData in userData:
-				print eachData
+			singleData = {}
+			singleData['twitter_text'] = re.sub(r'[^\x00-\x7f]',r'', eachData.full_text.encode("utf-8")) # remove hexadecimal characters
+			singleData['url'] = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', singleData['twitter_text']) #find all URLS - iframe URLS in this case
+			singleData['created_at'] = str(eachData.created_at.date())
+			singleData['id'] = str(eachData.id_str.encode("utf-8"))
+			for url in singleData['url']:
+				singleData['twitter_text'] = singleData['twitter_text'].replace(url,'').strip()  # Remove all URLS from text as we wont display it.
+			dataToSend['results'].append(singleData)
+
 		except:
 			print(sys.exc_info()[0])
-		return dataToSend
+
+	return dataToSend
 
 def get_data_from_cnn(query,start):
 	dataToSend = {}
@@ -96,16 +84,15 @@ def get_data_from_cnn(query,start):
 		start = 1
 	if query is None:
 			query = 'donald trump'
-	if start == 1:
-		url = 'https://search.api.cnn.io/content?q='+query+'&size=25'  # Before scraping! I decided to analyse the request flow of the CNN website and there I found this API which fetches all the news data for the page.
+	if start == 1:  # for Load more logic this will hit for the first occurance only
+		url = 'https://search.api.cnn.io/content?q='+query+'&size=25'  # Before scraping! I decided to analyse the request flow of the CNN website and there I found this API which fetches first part(ie size=25) of the news data for the page.
 	else:
 		fromNo = ((int(start)-1)*25)+1
-		url = 'https://search.api.cnn.io/content?q='+query+'&size=25&page='+str(start)+'&from='+str(fromNo) # Little more examining and found the pagination logic.
-	print url
+		url = 'https://search.api.cnn.io/content?q='+query+'&size=25&page='+str(start)+'&from='+str(fromNo) # This is the load more api.
 	try:
-		query_data = requests.get(url)
-		query_data = json.loads(query_data.content)
-		dataToSend['meta'] = query_data['meta']
+		query_data = requests.get(url)  #Simple Http request to fetch data from CNN
+		query_data = json.loads(query_data.content)  #Fetching it as Json
+		dataToSend['meta'] = query_data['meta']    
 		dataToSend['results'] = []
 	except: 
 		dataToSend['error_message'] = 'Coudnt connect to twitter'
